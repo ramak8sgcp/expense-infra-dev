96df1ac1d1e250a0d25455fd1d93c6ccbbf28151
ramak8sgcp <ramak8sgcp@gmail.com>
2025-02-06 07:15:18 +0530

    terraform

--- 50-app-alb/main.tf
+++ 50-app-alb/main.tf
@@ -1,12 +1,12 @@
 module "app_alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  internal = true
-  name = "${local.resource_name}-app-alb" #expense-dev-app-alb
-  vpc_id = local.vpc_id
-  subnets = local.private_subnet_ids
-  security_groups = [data.aws_ssm_parameter.app_alb_sg_id.value]
-  create_security_group = false
+  internal                   = true
+  name                       = "${local.resource_name}-app-alb" #expense-dev-app-alb
+  vpc_id                     = local.vpc_id
+  subnets                    = local.private_subnet_ids
+  security_groups            = [data.aws_ssm_parameter.app_alb_sg_id.value]
+  create_security_group      = false
   enable_deletion_protection = false
   tags = merge(
     var.common_tags,
@@ -15,35 +15,35 @@ module "app_alb" {
 }
 
 resource "aws_lb_listener" "http" {
-    load_balancer_arn = module.app_alb.arn
-    port = "80"
-    protocol = "HTTP"
+  load_balancer_arn = module.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-    default_action {
-      type = "fixed-response"
+  default_action {
+    type = "fixed-response"
 
-      fixed_response {
-        content_type = "text/html"
-        message_body = "<h1>Hello, I am from Application ALB</h1>"
-        status_code = "200"
-      }
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>Hello, I am from Application ALB</h1>"
+      status_code  = "200"
     }
+  }
 }
 
 module "records" {
-    source = "terraform-aws-module/route53/aws//modules/records"
+  source = "terraform-aws-module/route53/aws//modules/records"
 
-    zone_name = var.zone_name #ramana3490.online
-    records = [
-        {
-            name = "*.app-${var.environment}" # *.app-dev
-            type = "A"
-            alias = {
-                name = module.app_alb.db_subnet_group_name
-                zone_id = module.app_alb.zone_id # This belongs ALB internal hosted zone, not ours
-            }
-            allow_overwrite = true
-        }
-    ]
+  zone_name = var.zone_name #ramana3490.online
+  records = [
+    {
+      name = "*.app-${var.environment}" # *.app-dev
+      type = "A"
+      alias = {
+        name    = module.app_alb.db_subnet_group_name
+        zone_id = module.app_alb.zone_id # This belongs ALB internal hosted zone, not ours
+      }
+      allow_overwrite = true
+    }
+  ]
 }
   
